f589404b02091a4da64ddaffcabf82cb52bc77ba
Helder650 <marreirohedler34@gmail.com>
2025-08-29 08:31:32 -0300

    Add files via upload

---
+++ class/Usuario.js
@@ -0,0 +1,37 @@
+class Usuario {
+    static totalUsuarios = 0
+    #ativo
+    constructor(nome, idade, email) {
+        this.nome = nome
+        this.idade = idade
+        this.email = email
+        this.#ativo = false
+        Usuario.totalUsuarios++
+        
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    set ativo(ativo) {
+        this.#ativo = !!ativo
+    }
+
+    get boasVindas() {
+        return `Bem-vindo, ${this.nome}!`
+    }
+
+    mostrarDados() {
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Idade: ${this.idade}`)
+        console.log(`Email: ${this.email}`)
+        console.log(`Ativo: ${this.#ativo}`)
+    }
+
+    static contarUsuarios() {
+        console.log(`Total de usuários criados: ${Usuario.totalUsuarios}`)
+    } 
+}
+
+module.exports = Usuario
---
+++ index.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")()
+const Usuario = require("./class/Usuario")
+
+const nome = prompt("Digite o nome do Usuário: ")
+const idade = parseInt(prompt("Digite a idade do Usuário: "))
+const email = prompt("Digite o email do Usuário: ")
+const ativo = prompt("Usuário ativo? [S/N]: ")
+
+if (ativo !== "S" && ativo !== "N") {
+    console.log("Dado incorreto, saindo do sistema!")
+    process.exit()
+}
+
+const usuario = new Usuario(nome, idade, email)
+usuario.ativo = ativo === "S"
+
+console.log(usuario.boasVindas)
+usuario.mostrarDados()
+console.log("==========================")
+
+Usuario.contarUsuarios()
